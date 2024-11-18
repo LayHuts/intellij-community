1be6341e0fd5d2bf28e3c0457f8dace63fb53f63
Jaebaek Seo <jaebaek@google.com>
2024-11-18 14:42:43 -0800

    Add extension point for custom compiler plugin configuration
    When we register a compiler plugin via
    KotlinBundledFirCompilerPluginProvider, we cannot set the compiler
    plugin configuration. For some compiler plugins, setting the wrong
    compiler plugin configuration is critical. For example, the parcelize
    compiler plugin has a plugin option `USE_FIR`. If we does not set it,
    it chooses the default value i.e., `false` and causes issues on IR
    generation. This commit add K2CompilerPluginConfigurationProviderOnIDE
    that allows us to set the compiler plugin configuration. We add a
    default custom compiler plugin configuration provider for the parcelize
    compiler plugin (DefaultParcelizeCompilerPluginConfigurationProvider)
    to set `USE_FIR` option correctly.
    
    ^KTIJ-31698

--- plugins/kotlin/fir/src/org/jetbrains/kotlin/idea/fir/extensions/KtCompilerPluginsProviderIdeImpl.kt
+++ plugins/kotlin/fir/src/org/jetbrains/kotlin/idea/fir/extensions/KtCompilerPluginsProviderIdeImpl.kt
@@ -190,7 +190,10 @@ internal class KtCompilerPluginsProviderIdeImpl(
 
             with(pluginRegistrar) {
                 try {
-                    storage.registerExtensions(compilerConfiguration)
+                    val configuration = K2CompilerPluginConfigurationProviderOnIDE.getCompileConfigurationWithCustomOptions(
+                        pluginRegistrar, compilerConfiguration
+                    ) ?: compilerConfiguration
+                    storage.registerExtensions(configuration)
                 }
                 catch (e : ProcessCanceledException) {
                     throw e
